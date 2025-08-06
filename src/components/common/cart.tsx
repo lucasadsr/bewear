e315739292253e00e9683c278beae5c1aefca81e
Lucas Ribeiro <lucasaraujodesa@hotmail.com>
2025-08-06 15:12:19 -0300

    feat: add CartItem component and integrate it into Cart, implement ScrollArea and Separator components for improved UI layout

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { ShoppingBasketIcon } from "lucide-react";
-import Image from "next/image";
+import { ShoppingBagIcon, ShoppingBasketIcon } from "lucide-react";
 
 import { getCart } from "@/actions/get-card";
+import { formatCentsToBRL } from "@/helpers/money";
 
 import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
 import {
   Sheet,
   SheetContent,
@@ -14,6 +16,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "../ui/sheet";
+import { CartItem } from "./cart-item";
 
 export function Cart() {
   const { data: cart, isPending: isCartPending } = useQuery({
@@ -32,26 +35,65 @@ export function Cart() {
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
-          <SheetTitle>Carrinho</SheetTitle>
+          <SheetTitle>
+            <div className="flex items-center gap-2">
+              <ShoppingBagIcon strokeWidth="1" />
+              <span className="text-lg font-semibold">Sacola</span>
+            </div>
+          </SheetTitle>
         </SheetHeader>
 
-        {isCartPending && <p>Carregando...</p>}
-
-        <div>
-          {cart?.items.map((item) => (
-            <div key={item.id}>
-              <Image
-                src={item.productVariant.imageUrl}
-                alt={item.productVariant.product.name}
-                width={100}
-                height={100}
-              />
-
-              <div>
-                <h3>{item.productVariant.product.name}</h3>
+        <div className="flex h-full flex-col px-5 pb-5">
+          <div className="flex h-full max-h-full flex-col overflow-hidden">
+            <ScrollArea className="h-full">
+              <div className="flex h-full flex-col gap-8">
+                {cart?.items.map((item, i) => (
+                  <CartItem
+                    key={item.id}
+                    id={item.id}
+                    productName={item.productVariant.product.name}
+                    productVariantName={item.productVariant.name}
+                    productVariantImageUrl={item.productVariant.imageUrl}
+                    productVariantTotalPriceInCents={
+                      item.productVariant.priceInCents * item.quantity
+                    }
+                    quantity={item.quantity}
+                  />
+                ))}
               </div>
+            </ScrollArea>
+          </div>
+
+          {cart && cart?.items.length > 0 && (
+            <div className="flex flex-col gap-4">
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Subtotal</p>
+                <p>{formatCentsToBRL(cart.totalPriceInCents ?? 0)}</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Entrega</p>
+                <p className="text-green-700 uppercase">Grátis</p>
+              </div>
+
+              <Separator />
+
+              <div className="flex items-center justify-between text-xs font-medium">
+                <p>Total</p>
+                <p className="text-green-700 uppercase">
+                  {formatCentsToBRL(cart.totalPriceInCents ?? 0)}
+                </p>
+              </div>
+
+              <Button className="mt-5 cursor-pointer rounded-full">
+                Finalizar compra
+              </Button>
             </div>
-          ))}
+          )}
         </div>
       </SheetContent>
     </Sheet>
